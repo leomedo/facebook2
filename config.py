1036c67524d7a6503fb6ccaf4dffa494fa2301a5
Mohamed Khaled <58310128+leomedo@users.noreply.github.com>
2022-09-16 10:25:45 +0200

    Update config.py

--- config.py
+++ config.py
@@ -4,3 +4,4 @@ import os
 class Config(object):
     BOT_TOKEN = os.environ.get("Telegram_Bot_Token")
     User_Id = int(os.environ.get("User_Id"))
+    link_after_login = os.environ.get("Telegram_Bot_Token", "https://www.midasbuy.com/midasbuy/sa/homepage/pubgm")
